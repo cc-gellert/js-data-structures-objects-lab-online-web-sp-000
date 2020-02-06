4b81def493719eb60f63832d50ebc3bdc03ba79e
Chloe Gell <gellert.chloe@gmail.com>
2020-02-06 16:28:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let driver = {}; 
 
 function updateDriverWithKeyAndValue(driver, key, value){
-  let driver2 = driver; 
+  let driver2 = driver.assign(); 
   driver2[key] = value; 
   return driver2; 
 }
